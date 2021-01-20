3837d283f98626381e1337cfb0f73ddf26216ba9
nguyen-jason-01 <nguyen-jason-01@users.noreply.github.com>
2021-01-20 14:45:46 -0500

    New function
    Just added a function to return url

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
+import os
 
+#This program will use mangakakalot to get manga from because I don't know how to use mangadex
 
+#Get url to search
+def urlReturn(searchQuery):
+    searchQuery = searchQuery.replace(" ","_")
+    url = f'https://mangakakalot.com/search/story/{searchQuery}'
+    return url
 
